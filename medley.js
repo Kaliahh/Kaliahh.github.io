43d9489ef3ddfab3b49eadf0d62b7eb8a7ac0e26
Ane Jørgensen <anesoejoe@gmail.com>
2021-10-31 23:53:16 +0100

    Gnome and Insertion just look to similar

--- medley.js
+++ medley.js
@@ -39,42 +39,42 @@ async function setup() {
   };
 
   let algorithms = [
-    // async () => {
-    //   runningAlgortihm = "Merge";
-    //   await MergeSort(values, 0, values.length - 1);
-    // },
-    // async () => {
-    //   runningAlgortihm = "Quick";
-    //   await QuickSort(values, 0, values.length - 1);
-    // },
-    // async () => {
-    //   runningAlgortihm = "Fast Bubble";
-    //   await BubbleSort(values);
-    // },
-    // async () => {
-    //   runningAlgortihm = "Heap";
-    //   await HeapSort(values)
-    // },
+    async () => {
+      runningAlgortihm = "Merge";
+      await MergeSort(values, 0, values.length - 1);
+    },
+    async () => {
+      runningAlgortihm = "Quick";
+      await QuickSort(values, 0, values.length - 1);
+    },
+    async () => {
+      runningAlgortihm = "Fast Bubble";
+      await BubbleSort(values);
+    },
+    async () => {
+      runningAlgortihm = "Heap";
+      await HeapSort(values)
+    },
     async () => {
       runningAlgortihm = "Insertion";
       await InsertionSort(values);
     },
-    // async () => {
-    //   runningAlgortihm = "Selection";
-    //   await SelectionSort(values);
-    // },
-    // async () => {
-    //   runningAlgortihm = "Shell";
-    //   await ShellSort(values);
-    // },
-    // async () => {
-    //   runningAlgortihm = "OddEven";
-    //   await OddEvenSort(values);
-    // },
     async () => {
-      runningAlgortihm = "Gnome";
-      await GnomeSortOpt(values);
+      runningAlgortihm = "Selection";
+      await SelectionSort(values);
+    },
+    async () => {
+      runningAlgortihm = "Shell";
+      await ShellSort(values);
+    },
+    async () => {
+      runningAlgortihm = "OddEven";
+      await OddEvenSort(values);
     }
+    // async () => {
+    //   runningAlgortihm = "Gnome";
+    //   await GnomeSortOpt(values);
+    // },
     // async () => {
     //   runningAlgortihm = "Cycle";
     //   await CycleSort(values);
