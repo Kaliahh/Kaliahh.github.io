0d4a327c5f03deea0ef3dbf14bbff5b3af3a4ef7
Ane Jørgensen <anesoejoe@gmail.com>
2022-04-28 21:03:16 +0200

    Changed link a bit

--- src/main.js
+++ src/main.js
@@ -5,4 +5,4 @@ import router from './router'
 createApp(App).use(router).mount('#app')
 
 // Go through steps 2-5 to deploy
-// https://learnvue.co/2020/09/how-to-deploy-your-vue-app-to-github-pages/#step-3-run-git-add-dist-git-commit-m-adding-dist-subtree
+// https://learnvue.co/2020/09/how-to-deploy-your-vue-app-to-github-pages/#step-2-build-your-project-using-npm-build
