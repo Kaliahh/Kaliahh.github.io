6b85f6b72f3b77c26cbfa4c114bd9c8d23cfdf9a
Ane Jørgensen <anesoejoe@gmail.com>
2022-04-28 20:23:38 +0200

    Added source to deploy to gh-pages

--- src/main.js
+++ src/main.js
@@ -3,3 +3,6 @@ import App from './App.vue'
 import router from './router'
 
 createApp(App).use(router).mount('#app')
+
+// Go through steps 2-5 to deploy
+// https://learnvue.co/2020/09/how-to-deploy-your-vue-app-to-github-pages/#step-3-run-git-add-dist-git-commit-m-adding-dist-subtree
